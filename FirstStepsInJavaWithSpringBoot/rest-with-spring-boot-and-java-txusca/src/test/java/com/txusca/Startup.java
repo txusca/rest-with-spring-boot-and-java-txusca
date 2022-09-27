e606d306c15866e80ba47fcdbb1dd00cfb1e7dc1
Rafael Vicente <rafaelpv15@gmail.com>
2022-09-27 12:13:53 -0300

    First Steps With Java in SpringBoot3

---
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-txusca/src/test/java/com/txusca/Startup.java
@@ -0,0 +1,13 @@
+package com.txusca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Startup {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
