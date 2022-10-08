d2cc746551bb6a6c6ffc301d3d54c9054e70465d
Rafael Vicente <rafaelpv15@gmail.com>
2022-10-08 19:13:57 -0300

    Content Negociation with json, xml and yaml

--- FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-txusca/src/main/java/com/txusca/controllers/PersonController.java
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-txusca/src/main/java/com/txusca/controllers/PersonController.java
@@ -2,8 +2,8 @@ package com.txusca.controllers;
 
 import java.util.List;
 
+import com.txusca.utils.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,22 +27,24 @@ public class PersonController {
     @Autowired
     private PersonServices services;
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
     public Person findById(@PathVariable(name = "id") Long id) {
         return services.findById(id);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
     public List<Person> findAll() throws Exception {
         return services.findAll();
     }
 
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
     public Person create(@RequestBody Person person) {
         return services.create(person);
     }
 
-    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
     public Person update(@RequestBody Person person) {
         return services.create(person);
     }
@@ -52,6 +54,6 @@ public class PersonController {
         services.delete(id);
         return ResponseEntity.noContent().build();
     }
-    
+
 
 }
